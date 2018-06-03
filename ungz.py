8bfb15b82141fc450932ab081dfcc4f3ad42c0e1
lsqlhl5862 <lsqlhl5862@foxmail.com>
2018-06-03 22:34:18 +0800

    Fix storage path

---
+++ ungz.py
@@ -0,0 +1,14 @@
+import gzip
+import os
+def un_gz(file_name):
+    """ungz zip file"""
+    f_name = file_name.replace(".gz", "")
+    #获取文件的名称，去掉
+    g_file = gzip.GzipFile(file_name)
+    #创建gzip对象
+    open(f_name, "wb+").write(g_file.read())
+    #gzip对象用read()打开后，写入open()建立的文件里。
+    g_file.close()
+    #关闭gzip对象
+
+un_gz("patch-3.18.32.gz")
